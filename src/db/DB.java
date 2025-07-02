0cf8e72eef45d16c19add563fda3686aa1800578
Kayo César <antkayocesrdev@gmail.com>
2025-07-02 10:37:04 -0300

    Reusing instantiation

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,60 +10,61 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-
-	private static Connection conn = null;
 	
-	public static Connection getConnection() {
-		if (conn == null) {
+	private static Connection conn = null;
+	public static Connection getConnection(){
+		if(conn == null){
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
-				conn = DriverManager.getConnection(url, props);
-			}
-			catch (SQLException e) {
+				conn = DriverManager.getConnection(url ,props);
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
-			}
+			}{}
+
 		}
 		return conn;
-	}
+	} 
 	
-	public static void closeConnection() {
-		if (conn != null) {
+	public static void closeConnection(){
+		if(conn != null){
 			try {
 				conn.close();
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
+		
 	}
 	
-	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
+	
+	private static Properties loadProperties(){
+		try(FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}
-		catch (IOException e) {
+		}catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
 	
-	public static void closeStatement(Statement st) {
-		if (st != null) {
+	public static void closeStatement(Statement st){
+		if(st != null){
 			try {
 				st.close();
 			} catch (SQLException e) {
-				throw new DbException(e.getMessage());
+				throw new DbException(e.getLocalizedMessage());
 			}
 		}
 	}
+	
 
 	public static void closeResultSet(ResultSet rs) {
-		if (rs != null) {
+		if(rs != null){
 			try {
 				rs.close();
 			} catch (SQLException e) {
-				throw new DbException(e.getMessage());
+				throw new DbException(e.getLocalizedMessage());
 			}
 		}
 	}
